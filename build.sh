4015f4e415e418657b68fda6431b9f2edf32b9ac
Tobin Cooney <tobin@tobincooney.com>
2023-07-10 11:59:26 -0400

    indicate date post last edited

--- build.sh
+++ build.sh
@@ -32,6 +32,12 @@ posts_toc=""
 for i in $( ls -r src/posts/*.htm ); do
 	name=$( echo "$i" | sed 's/src\/posts\///' | sed 's/\.htm//' )
 	echo "-- $name"
+	modified_raw="$( git log --pretty=format:'%ci' -- $i | head -1 | sed 's/^20//' )"
+	modified="${modified_raw:0:8}"
+	mod_string=""
+	if [ "$name" != "$modified" ]; then
+		mod_string=" <i class='edited'>(edited $modified)</i>"
+	fi
 	prettyname=$( head -3 $i | tail -1 | sed 's/<h2>//' | sed 's/<\/h2>//' )
 	author=$( head -2 $i | tail -1 )
 	author_format=$( echo "${author,,}" | sed 's/ /_/' )
@@ -43,11 +49,11 @@ for i in $( ls -r src/posts/*.htm ); do
 		echo "<li><a href=\"$name.html\">$prettyname</a></li>" >> etc/tags/$n.txt
 	done
 	#main="<h1>$prettyname</h1>$( tail -n +3 $i )"
-	main="<p><a href='posts.html#$name'>&lt; Back</a></p><center><small>$name</small></center><h1>$prettyname</h1><p class='author'>by <a href='$author_format.html'>$author</a></p>$( tail -n +4 $i )"
+	main="<p><a href='posts.html#$name'>&lt; Back</a></p><center><small>$name$mod_string</small></center><h1>$prettyname</h1><p class='author'>by <a href='$author_format.html'>$author</a></p>$( tail -n +4 $i )"
 	# standalone page
 	page="$head$main<br><p class='tags'>Tags: $tags</p>$foot"
 	posts_toc+="<li><small>$name</small> <a href='#$name'>$prettyname</a></li>"
-	posts_full+="<br><a id=\"$name\"></a><div class='post'><small>$name (<a href='$author_format.html'>$author</a>)</small><br>$( tail -n +3 $i | sed 's/<h2>/<h2><a href='$name.html'>/' | sed 's/<\/h2>/<\/a><\/h2>/' )<br><p class='tags'>Tags: $tags</p></div>"
+	posts_full+="<br><a id=\"$name\"></a><div class='post'><small>$name$mod_string<br>by <a href='$author_format.html'>$author</a></small><br>$( tail -n +3 $i | sed 's/<h2>/<h2><a href='$name.html'>/' | sed 's/<\/h2>/<\/a><\/h2>/' )<br><p class='tags'>Tags: $tags</p></div>"
 	echo "$page" > site/$name.html
 done
 
